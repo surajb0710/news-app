253df109053b05f3d9cfc27ed552e3457f9a7493
Suraj <104007414+surajb0710@users.noreply.github.com>
2024-07-11 00:24:35 +0530

    Minor fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export class Navbar extends Component {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <Link className="navbar-brand" to="/">
-              Navbar
+              News App
             </Link>
             <button
               className="navbar-toggler"
